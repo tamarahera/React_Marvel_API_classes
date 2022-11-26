a9c0500d312a2d0185b1ab5cd8fc31f4ac9fcfba
tamara_hera <tamara.herashchenko@gmail.com>
2022-11-26 19:09:52 +0100

    Add random character

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import MarvelService from './services/MarvelService';
+
 import './style/style.scss';
-
-
-const marverlService = new MarvelService();
-
-marverlService.getAllCharacters().then(res => console.log(res));
-marverlService.getCharacter(1011052).then(res => console.log(res));
-
-marverlService.getAllCharacters().then(res => console.log(res.data.results.forEach(item => console.log(item.name))));
 
 
 ReactDOM.render(
@@ -18,5 +10,4 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
+);  
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,7 +2,7 @@ class MarvelService {
     _apiBase = `https://gateway.marvel.com:443/v1/public/`; //лодаш неформально означає що ми не можемо змінювати ці дані
     _apiKey = `apikey=03bceba952252af3375580476ec6cb07`;
 
-    getResourse = async (url) => {
+    getResource = async (url) => {
         let res = await fetch(url);
     
         if (!res.ok) {
@@ -12,12 +12,24 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter())
     }
 
-    getCharacter = (id) => {
-        return this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
